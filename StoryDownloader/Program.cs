16aec12242f9da1b88c453d220f5b4238cb416b6
Heiner <heiner.suter@gmx.ch>
2016-02-01 15:36:55 +0100

    initial

---
+++ StoryDownloader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using StoryDownloader.Web;
+
+namespace StoryDownloader
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Browser().DownloadStroy();
+            Console.Read();
+        }
+    }
+}
---
+++ StoryDownloader/Web/Browser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+
+namespace StoryDownloader.Web
+{
+    public class Browser
+    {
+        public void DownloadStroy()
+        {
+            IWebDriver driver = new PhantomJSDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/");
+
+
+            var query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Cheese");
+            query.Submit();
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => d.Title.ToLower().StartsWith("cheese"));
+
+            Write("Page title is: " + driver.Title);
+
+            driver.Quit();
+        }
+
+        private static void Write(string message)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
